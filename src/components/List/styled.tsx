a151178f89fbad1b707c6a945cc363f79079d27f
fidel miranda <fidel.miranda@dspot.com.pl>
2023-02-28 18:43:58 -0500

    add styles to List Component

---
+++ src/components/List/styled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonDelete = styled.button`
+  color: white;
+  font-weight: 400;
+  font-size: 16px;
+  background-color: rgb(225 29 72);
+  padding: 10px;
+  border: 2px solid white;
+  border-radius: 10px;
+`;
+
+export const ButtonEdit = styled.button`
+  color: white;
+  font-weight: 400;
+  font-size: 16px;
+  background-color: rgb(16 185 129);
+  padding: 10px;
+  border: 2px solid white;
+  border-radius: 10px;
+`;
