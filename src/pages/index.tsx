dd1ff60a4e5be7c412239e5dfd7f471f2b2cd17e
fidel miranda <fidel.miranda@dspot.com.pl>
2023-02-28 18:45:29 -0500

    update files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-
+import { List } from "@/components/List/List";
 export default function Home() {
   return (
     <>
@@ -12,32 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-          </p>
-          <div>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className="flex border-2 border-l-purple-200">
-          <div className="flex-none w-32"><h1>01</h1></div>
-          <div className="flex-initial w-32 ...">02</div>
-          <div className="flex-initial w-32 ...">03</div>
-        </div>
+        
+        <List/>
+      
       </main>
     </>
   );
