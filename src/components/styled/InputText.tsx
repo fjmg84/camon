8d646183e42a4a3dc36f0383cee71402eeb0102b
fidel miranda <fidel.miranda@dspot.com.pl>
2023-03-01 04:01:42 -0500

    add and remove old and new styles

---
+++ src/components/styled/InputText.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const InputText = styled.input`
+  color: white;
+  font-weight: 400;
+  font-size: 16px;
+  padding: 20px;
+  margin: 20px;
+  border: 2px solid white;
+  width: 100%;
+
+`;
