0b801e1fa85c8f3586b5d4e2f9ec1b3908a72fca
fidel miranda <fidel.miranda@dspot.com.pl>
2023-03-02 14:07:27 -0500

    refactoring List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,69 +1,71 @@
-import Image from "next/image";
-import Link from "next/link";
-import { ButtonDelete, ButtonEdit } from "../styled/Buttons";
-import { StyledLink } from "../styled/Link";
-import styles from "./styles.module.css";
+import { useRouter } from "next/router";
 import { Books } from "@/components/interfaces/Book";
 import Cover from "../Common/Cover/Cover";
+import { DELETE_BOOK_FORM } from "@/query/delete";
+import { useMutation } from "@apollo/client";
+import { ButtonDelete } from "../styled/Buttons";
+import { StyledLink } from "../styled/Link";
+import styles from "./styles.module.css";
+import Details from "../Common/Details/Details";
 
 const List = ({ books }: { books: Books }) => {
-  const deleteBook = (id: string) => {
-    console.log(id);
+  const { reload } = useRouter();
+  const [deleteBookById, { loading, data }] =
+    useMutation(DELETE_BOOK_FORM);
+
+  if (data !== undefined && !loading) {
+    alert("Delete book successfully")
+    reload();
+  }
+
+  const deleteBook = (id: string | undefined) => {
+    if (id) deleteBookById({ variables: { id } });
   };
 
   return (
     <>
       <h1>List of the Books</h1>
-      <div className={styles.container}>
-        {books.map((book) => {
-          const { id, title, cover, author, year, ISBN, description } = book;
-          return (
-            <div key={id} className={styles.book}>
-              {cover && (
-                <Cover
-                  style={styles.image}
-                  alt={cover}
-                  url={cover}
-                  width={200}
-                  height={400}
-                />
-              )}
+      {loading && <h1>Loading Data...</h1>}
+      {books.length > 0 && !loading ? (
+        <div className={styles.container}>
+          {books.map((book) => {
+            const { id, title, cover, author, year, ISBN, description } = book;
+            return (
+              <div key={id} className={styles.book}>
+                {cover && (
+                  <Cover
+                    style={styles.image}
+                    alt={cover}
+                    url={cover}
+                    width={200}
+                    height={400}
+                  />
+                )}
 
-              <div className={styles.box_details}>
-                <ul className={styles.details_book}>
-                  <li>
-                    <h6>Title:</h6>
-                    <span>{title}</span>
-                  </li>
-                  <li>
-                    <h6>Author:</h6>
-                    <span>{author}</span>
-                  </li>
-                  <li>
-                    <h6>Year:</h6>
-                    <span>{year}</span>
-                  </li>
-                  <li>
-                    <h6>ISBN:</h6>
-                    <span>{ISBN}</span>
-                  </li>
-                  <li>
-                    <h6>Description:</h6>
-                    <span>{description}</span>
-                  </li>
-                </ul>
-              </div>
+                <div className={styles.box_details}>
+                  <Details
+                    id={id}
+                    title={title}
+                    author={author}
+                    year={year}
+                    ISBN={ISBN}
+                    description={description}
+                  />
+                </div>
 
-              <div className={styles.buttons}>
-                <StyledLink href={`edit/${id}`}>Edit</StyledLink>
-                <ButtonDelete onClick={() => deleteBook(id)}>
-                  Delete
-                </ButtonDelete>
+                <div className={styles.buttons}>
+                  <StyledLink href={`edit/${id}`}>Edit</StyledLink>
+                  <ButtonDelete onClick={() => deleteBook(id)}>
+                    Delete
+                  </ButtonDelete>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <h2>Books not found</h2>
+      )}
     </>
   );
 };
