cd6700a50228125c41bf3ee4d32579248f8dc14b
fidel miranda <fidel.miranda@dspot.com.pl>
2023-03-02 03:08:35 -0500

    add styled Button

--- src/components/styled/Buttons.tsx
+++ src/components/styled/Buttons.tsx
@@ -8,6 +8,7 @@ export const ButtonDelete = styled.button`
   padding: 10px;
   border: 2px solid white;
   border-radius: 10px;
+  cursor: pointer;
 `;
 
 export const ButtonEdit = styled.button`
@@ -18,4 +19,17 @@ export const ButtonEdit = styled.button`
   padding: 10px;
   border: 2px solid white;
   border-radius: 10px;
+  cursor: pointer;
+`;
+
+export const ButtonSelectImage = styled.button`
+  color: white;
+  font-weight: 400;
+  font-size: 16px;
+  border: 2px solid white;
+  padding: 10px;
+  background-color: rgb(16 185 129);
+  border: 2px solid white;
+  border-radius: 10px;
+  cursor: pointer;
 `;
