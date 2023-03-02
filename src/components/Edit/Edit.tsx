9e54589f7869ed877222941c751a9cfcfc0ee2a7
fidel miranda <fidel.miranda@dspot.com.pl>
2023-03-02 14:06:23 -0500

    add management action with Cover component

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -1,20 +1,26 @@
 import { Book } from "@/components/interfaces/Book";
 import styles from "./styles.module.css";
 import { useMutation } from "@apollo/client";
-import { UPDATE_BOOK_FORM } from "@/query/update";
+import { UPDATE_BOOK_FORM, UPDATE_BOOK_IMAGE } from "@/query/update";
 import Cover from "../Common/Cover/Cover";
 import Form from "../Common/Form/Form";
 
 const Edit = ({ book }: { book: Book | undefined }) => {
-
-  const [updateBookForm, { loading, error, data }] = useMutation(UPDATE_BOOK_FORM);
+  const [updateBookForm] = useMutation(UPDATE_BOOK_FORM);
+  const [updateBookImage] = useMutation(UPDATE_BOOK_IMAGE);
 
   return (
     <>
       <h1>Edit Book</h1>
       <div className={styles.container}>
-        <Cover idBook={book?.id} url={book?.cover} alt={book?.cover} edit={true} />
-        <Form handleAction={updateBookForm} book={book} />
+        <Cover
+          handleImage={updateBookImage}
+          idBook={book?.id}
+          url={book?.cover}
+          alt={book?.cover}
+          edit={true}
+        />
+        <Form type={"update"} handleAction={updateBookForm} book={book} />
       </div>
     </>
   );
